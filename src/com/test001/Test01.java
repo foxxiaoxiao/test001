a3b9461cb3c624bde7b17af304b3152ceac649d4
foxxiaoxiao <foxxiaoxiao@163.com>
2019-07-04 13:57:39 +0800

    dev2.3.

--- src/com/test001/Test01.java
+++ src/com/test001/Test01.java
@@ -2,4 +2,7 @@ package com.test001;
 
 public class Test01 {
 
+	public static void main(String[] args) {
+		
+	}
 }
--- src/com/test001/Test02.java
+++ src/com/test001/Test02.java
@@ -2,4 +2,7 @@ package com.test001;
 
 public class Test02 {
 
+	public static void main(String[] args) {
+		
+	}
 }
